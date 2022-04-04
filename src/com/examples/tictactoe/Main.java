6a7e32b6ecdd3c0b861d3b2a6e38e1f60c48f95a
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-04-04 09:03:13 +0530

    Added: UC2 - Allow player to choose

--- src/com/examples/tictactoe/Main.java
+++ src/com/examples/tictactoe/Main.java
@@ -4,5 +4,6 @@ public class Main {
        public static void main(String[] args) {
            TicTacToeGame ticTacToeGame = new TicTacToeGame();
            ticTacToeGame.createBoard();
+           ticTacToeGame.getPlayerChoice();
     }
 }
--- src/com/examples/tictactoe/TicTacToeGame.java
+++ src/com/examples/tictactoe/TicTacToeGame.java
@@ -1,12 +1,34 @@
 package com.examples.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     char[] board = new char[10];
+    static char player, computer;
+    static Scanner sc = new Scanner(System.in);
 
     public char[] createBoard() {
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
         return board;
+    }
+
+    public void getPlayerChoice() {
+        System.out.println("Choose between X or O : ");
+        String input = sc.nextLine();
+        player = Character.toUpperCase(input.charAt(0));
+
+        if (player == 'X') {
+            computer = 'O';
+        }
+        else if (player == 'O') {
+            computer = 'X';
+        }
+        else {
+            System.out.println("Invalid Choice");
+        }
+        System.out.println("Your choice : " + player);
+        System.out.println("Computer's choice : " + computer);
     }
 }
